20ddfc2095205f272f60279b84b69eda2a7fa8a2
Krystall Messam <massem100@outlook.com>
2020-02-10 23:21:46 -0500

    Started Step 3

--- app/views.py
+++ app/views.py
@@ -24,7 +24,9 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
+@app.route('/contact')
+def contact(): 
+    return render_template('contact.html')
 ###
 # The functions below should be applicable to all Flask apps.
 ###
